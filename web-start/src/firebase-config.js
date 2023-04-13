fabc2d4009d1e0799fd50998451473df3965a262
JC <jonathancavia0101@gmail.com>
2023-04-13 18:28:18 -0300

    paso 11

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -6,15 +6,19 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
+// Your web app's Firebase configuration
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyDl85SH6xjoBkIPu8UwPHV1S22HEmvkq4s",
+  authDomain: "tp2-arq-redes-servicios.firebaseapp.com",
+  projectId: "tp2-arq-redes-servicios",
+  storageBucket: "tp2-arq-redes-servicios.appspot.com",
+  messagingSenderId: "914786110860",
+  appId: "1:914786110860:web:cc0bc305c95ec11291daa9"
 };
 
+
+
+
 export function getFirebaseConfig() {
-  if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
-  } else {
-    return config;
-  }
+  return config;
 }
